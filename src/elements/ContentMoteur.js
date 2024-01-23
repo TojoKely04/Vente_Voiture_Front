33a761075f3e0f9bb15bbf5f701fdafa102aa36a
ilokely <soanaivojunior@gmail.com>
2024-01-23 22:49:12 +0100

    elements necessaire

--- src/elements/ContentMoteur.js
+++ src/elements/ContentMoteur.js
@@ -4,13 +4,83 @@ import Header from '../header/header';
 import { Label , Input, Form, FormGroup, Container , Button } from 'reactstrap';
 import './content.css'; 
 const ContentMoteur = () => {
+
+    const [groups , setGroups] = useState([]);
+
+    useEffect(() => {
+      async function fetchData() {
+        const result = await fetch('moteur');
+        const body = await result.json();
+        setGroups(body);
+      }
+      fetchData();
+    }, [])
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        const cat = form.elements.moteur.value;
+        try{
+            const formData = {
+                "moteur" : moteur
+            };
+            console.log(JSON.stringify(formData));
+            const response = await fetch('/moteur' , {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            } );
+            if (response.ok) {
+                async function fetchData() {
+                    const result = await fetch('moteur');
+                    const body = await result.json;
+                    setGroups(body);
+                }
+                fetchData();
+            }else{
+                console.error('Erreur lors de l\'insertion de l\'objet')
+            }
+        }catch(error){
+            alert('Erreur reseau :' , error);
+        }
+    };
+
+    const groupList = groups.map(group => {
+        return <tr>
+            <td> {group.moteur}{console.log(group)}</td>
+            <td> <a href="#">Modifier</a> </td>
+            <td> <button onClick={() => remove(group.idMoteur)}>Supprimer</button> </td>
+        </tr>
+        
+    });
+
+    const remove = async (id) => {
+        await fetch(`moteur/${id}`, {
+          method: 'DELETE',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }).then(() => {
+            async function fetchData() {
+                const result = await fetch(`moteur`);
+                const body = await result.json();
+                setGroups(body);
+            }
+            fetchData();
+        });
+    }
+
     return (
         <>
         <Header/>
         <Container>
         <div className="body--activity">
             <div className="">
-                <h1 className="ajout--title"> Ajouter Marque </h1>
+                <h1 className="ajout--title"> Ajouter moteur </h1>
                 <p> Moteur : <input type="text" name="InsertMoteur" id="" /> </p>
                 <button type="submit" id="boutton">Ajouter</button>
             </div>
@@ -22,11 +92,7 @@ const ContentMoteur = () => {
                         <td> Nom </td>
                         <td> Action </td>
                     </tr>
-                    <tr>
-                        <td> Nom Moteur </td>
-                        <td> <a href="#">Modifier</a> </td>
-                        <td> <a href="#">Supprimer</a> </td>
-                    </tr>
+                    {groupList}
                 </table>  
             </div>
                 
